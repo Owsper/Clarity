96befce1dd92fe523508e23f382119c58f6560b4
Owsper <rohanftw2466@gmail.com>
2026-04-11 12:43:34 +0200

    Renamed CSS file and added functionality to popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,12 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+
+    console.log('Clarity is running');
+
+    const adBlock  = document.getElementById('adBlock');
+    const thirstTrapBlock = document.getElementById('thirstTrapBlock');
+    const nsfwBlock = document.getElementById('nsfwBlock');
+    const ragebaitBlock = document.getElementById('ragebaitBlock'); 
+
+
+});
